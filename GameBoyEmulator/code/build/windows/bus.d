e24397c44d69acfab9b53f63e2ee02f8568a3dbe
FrancescoBellino97 <bellinofrancesco4@gmail.com>
2025-08-03 21:38:27 +0200

    Removed build support files

--- GameBoyEmulator/code/build/windows/bus.d
+++
@@ -1,8 +0,0 @@
-build/windows/bus.o: app/src/bus.c app/inc/bus.h app/inc/common.h \
- app/inc/cart.h app/inc/ram.h app/inc/cpu.h app/inc/instructions.h
-app/inc/bus.h:
-app/inc/common.h:
-app/inc/cart.h:
-app/inc/ram.h:
-app/inc/cpu.h:
-app/inc/instructions.h:
--- GameBoyEmulator/code/build/windows/cpu.d
+++
@@ -1,8 +0,0 @@
-build/windows/cpu.o: app/src/cpu.c app/inc/cpu.h app/inc/common.h \
- app/inc/instructions.h app/inc/bus.h app/inc/emu.h app/inc/interrupts.h
-app/inc/cpu.h:
-app/inc/common.h:
-app/inc/instructions.h:
-app/inc/bus.h:
-app/inc/emu.h:
-app/inc/interrupts.h:
